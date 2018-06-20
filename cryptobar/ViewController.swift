9f02fdb047521abacaf22b60656610e6bfbccd61
Artur Komarov <artur.komarov@gmail.com>
2018-06-20 17:03:34 +0800

    Initial Commit

---
+++ cryptobar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cryptobar
+//
+//  Created by Артур Комаров on 20.06.2018.
+//  Copyright © 2018 Артур Комаров. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
